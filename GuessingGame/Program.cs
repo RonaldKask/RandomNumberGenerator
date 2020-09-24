e5479c8a56c742eec86f261be83728f1afee84cf
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:33:07 +0300

    asddsa

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int userGuess = Int32.Parse(Console.ReadLine());
+            bool correctGuess = false;
+
+            while(!correctGuess)
+            {
+                if(randomNumber == userGuess)
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+                }else
+                {
+                    if (userGuess > randomNumber)
+                    {
+                        Console.WriteLine("My number is smaller.");
+                        userGuess = Int32.Parse(Console.ReadLine());
+                    }else
+                    {
+                        Console.WriteLine("My number is bigger.");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int myRandomNumber = rnd.Next(1, 11);
+            //Console.WriteLine($"Your random number is {myRandomNumber}");
+            Console.Write("What is my number from 1 to 10: ");
+            int userGuess = Int32.Parse(Console.ReadLine());
+
+            if(myRandomNumber == userGuess)
+            {
+                Console.WriteLine("Congratulations! You've played me!");
+            }else
+            {
+                Console.WriteLine($"You lost! My random number was {myRandomNumber}");
+            }
+
+        }
+    }
+}
